360cfb6e8f830c5a8765078eef9cc268da2a064b
MonzerMahmoud <monzermahmoud97@gmail.com>
2024-01-21 16:28:54 +0400

    bug fixed

--- src/fake-data/fake-data.module.ts
+++ src/fake-data/fake-data.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FakeDataService } from './fake-data.service';
 import { FakeDataController } from './fake-data.controller';
+import { ParamTypeHandler } from 'src/handlers/param-type.handler';
 
 @Module({
+  imports: [ParamTypeHandler],
   controllers: [FakeDataController],
   providers: [FakeDataService],
 })
